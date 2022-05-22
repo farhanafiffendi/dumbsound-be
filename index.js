24aec9f2b5a32db137e0d27eb64eeb3cc1e3d4bd
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-05-22 18:33:58 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const express = require('express')
+// import dotenv and call config function to load environment
+require("dotenv").config();
+const express = require("express");
+
+const cors = require("cors");
+
+// import this
+const http = require("http");
+const { Server } = require("socket.io");
 
 // Get routes to the variabel
-const router = require('./src/routes')
-require('dotenv').config()
+const router = require("./src/routes");
 
-const cors = require('cors');
+const app = express();
 
-const app = express()
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // we must define cors because our client and server have diffe
+    },
+});
 
-const port = 5000
+// import socket function and call with parameter io
+require("./src/socket")(io);
+
+const port = 5000;
 
 app.use(express.json());
 app.use(cors());
 
 // Add endpoint grouping and router
-app.use('/api/v1/', router)
-app.use('/uploads', express.static('uploads'))
+app.use("/api/v1/", router);
+app.use("/uploads", express.static("uploads"));
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+// change app to server
+server.listen(port, () => console.log(`Listening on port ${port}!`));
